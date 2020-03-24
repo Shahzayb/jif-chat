1faad85223310291199ca65ffd7daa9f0438014c
shahzaib sarwar <imshahzayb@gmail.com>
2020-03-24 15:42:01 +0500

    added client routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      {/* <Navbar /> */}
+      <Switch>
+        <Route exact path="/" component={() => <div>home</div>} />
+        <Route path="/post" component={() => <div>post</div>} />
+        <Route path="/login" component={() => <div>login</div>} />
+        <Route path="/logout" component={() => <div>logout</div>} />
+      </Switch>
     </div>
   );
 }
