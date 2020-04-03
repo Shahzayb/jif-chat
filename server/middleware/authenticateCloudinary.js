f2c2cdc7fd1de2fd695d83d71b8a70585c56b833
shahzaib sarwar <imshahzayb@gmail.com>
2020-04-04 04:10:33 +0500

    fixed authenticateCoulinary middleware

--- server/middleware/authenticateCloudinary.js
+++ server/middleware/authenticateCloudinary.js
@@ -3,23 +3,23 @@ const sha1 = require('sha1');
 module.exports = (req, res, next) => {
   try {
     const reqData = JSON.stringify(req.body);
-    const reqTimestamp = req.headers['x-cld-timestamp'];
-    const apiSecret = process.env.CLOUDINARY_API_SECRET;
-    const reqSig = req.headers['x-cld-signature'];
+    const reqTimestamp = String(req.headers['x-cld-timestamp']);
+    const apiSecret = String(process.env.CLOUDINARY_API_SECRET);
+    const reqSig = String(req.headers['x-cld-signature']);
 
-    const computedSig = sha1(reqData + reqTimestamp + apiSecret);
+    const computedSig = String(sha1(reqData + reqTimestamp + apiSecret));
 
     console.log(reqData, reqTimestamp, apiSecret, reqSig, computedSig);
 
     if (computedSig === reqSig) {
       console.log('valid cloudinary request');
-      next();
+      return next();
     } else {
       console.log('invalid cloudinary request');
-      res.end({ msg: 'invalid cloudinary request' });
+      return res.end();
     }
-  } catch (error) {
-    console.log(error);
+  } catch (e) {
+    console.log(e);
     res.status(422).send();
   }
 };
